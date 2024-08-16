dda799928a292ef9bfe16c72a179278bddfc3718
Marina2022 <marusiiiia@yandex.ru>
2024-08-16 13:49:39 +0300

    fix

--- src/components/FavPage/FavList/FavList.jsx
+++ src/components/FavPage/FavList/FavList.jsx
@@ -5,8 +5,7 @@ const FavList = ({products, fullSize=false}) => {
   return (
     <ul className={fullSize ? s.listFull : s.list}>
       {
-        products.map((product, i) => {
-            // manyProducts.map((product, i) => {  // для тестирование - много карточек
+        products.map((product, i) => {            
             return <ProductCard key={i} isBigScreen={false} product={product}/>
           }
         )
