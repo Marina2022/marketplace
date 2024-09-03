e7fbf6db0a7e691fc18de01f82db341a5c963641
Marina2022 <marusiiiia@yandex.ru>
2024-09-03 17:56:49 +0300

    header dropdown on hover

--- src/components/layout/Header/UserDropdown/UserDropdown.jsx
+++ src/components/layout/Header/UserDropdown/UserDropdown.jsx
@@ -12,37 +12,37 @@ const UserDropdown = () => {
 
   const location = useLocation();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef(null);
-  const buttonRef = useRef(null);
+  // const dropdownRef = useRef(null);
+   const buttonRef = useRef(null);
 
   useEffect(() => {
     setIsDropdownOpen(false);
   }, [location]);
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (buttonRef.current && buttonRef.current.contains(event.target)) {
-        return;
-      }
-
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setIsDropdownOpen(false);
-      }
-    };
-    const handleEscapeKey = (event) => {
-      if (event.key === 'Escape') {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    document.addEventListener('keydown', handleEscapeKey);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-      document.removeEventListener('keydown', handleEscapeKey);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const handleClickOutside = (event) => {
+  //     if (buttonRef.current && buttonRef.current.contains(event.target)) {
+  //       return;
+  //     }
+  //
+  //     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+  //       setIsDropdownOpen(false);
+  //     }
+  //   };
+  //   const handleEscapeKey = (event) => {
+  //     if (event.key === 'Escape') {
+  //       setIsDropdownOpen(false);
+  //     }
+  //   };
+  //
+  //   document.addEventListener('mousedown', handleClickOutside);
+  //   document.addEventListener('keydown', handleEscapeKey);
+  //
+  //   return () => {
+  //     document.removeEventListener('mousedown', handleClickOutside);
+  //     document.removeEventListener('keydown', handleEscapeKey);
+  //   };
+  // }, []);
 
   let activeProfile;
 
@@ -60,43 +60,48 @@ const UserDropdown = () => {
   const openDropdownHandler = () => {
     setIsDropdownOpen(prev => !prev);
   };
-    
+
   const dispatch = useDispatch()
   const logoutHandler = () => {
     dispatch(logout())
   }
 
   return (
-    <div className={s.userDropdownWrapper}>
+    <div className={s.userDropdownWrapper}
+         onMouseEnter={() => setIsDropdownOpen(true)}
+       onMouseLeave={()=>setIsDropdownOpen(false)}
+    >
       <button
         ref={buttonRef}
-        onClick={openDropdownHandler}
         className={s.userDropdownBtn}
       >
         {letter}
       </button>
 
       {isDropdownOpen && (
-        <div ref={dropdownRef} className={s.dropdown}>
-          <div className={s.activeProfile}>
-            <div className={s.user}>
-              {userProfilesLoadingStatus === 'success' && <div className={s.letterInDropdown}>{letter}</div>}
-              {userProfilesLoadingStatus === 'success' &&
-                <div className={s.userName}>{activeProfile?.profileName}</div>}
+        // <div className={s.dropWrapper} ref={dropdownRef}>
+        <div className={s.dropWrapper} >
+          <div className={s.dropdown}>
+            <div className={s.activeProfile}>
+              <div className={s.user}>
+                {userProfilesLoadingStatus === 'success' && <div className={s.letterInDropdown}>{letter}</div>}
+                {userProfilesLoadingStatus === 'success' &&
+                  <div className={s.userName}>{activeProfile?.profileName}</div>}
+              </div>
+              <ul className={s.dropdownMenu}>
+                <li><Link className={s.dropdownLink} to="/lk">Личный кабинет</Link></li>
+                <li><Link className={s.dropdownLink} to="/orders">Заказы</Link></li>
+                <li><Link className={s.dropdownLink} to="/favourites">Избранное</Link></li>
+                <li><Link className={s.dropdownLink} to="/messages">Сообщения</Link></li>
+              </ul>
+              <Link className={s.companiesLink} to="/lk">Мои организации</Link>
+              <button onClick={logoutHandler} className={s.logoutBtn}>Выход</button>
             </div>
-            <ul className={s.dropdownMenu}>
-              <li><Link className={s.dropdownLink} to="/lk">Личный кабинет</Link></li>
-              <li><Link className={s.dropdownLink} to="/orders">Заказы</Link></li>
-              <li><Link className={s.dropdownLink} to="/favourites">Избранное</Link></li>
-              <li><Link className={s.dropdownLink} to="/messages">Сообщения</Link></li>
-            </ul>
-            <Link className={s.companiesLink} to="/lk">Мои организации</Link>
-            <button onClick={logoutHandler} className={s.logoutBtn}>Выход</button>
+            {userProfilesLoadingStatus === 'success' && userProfiles.length > 1 && (
+              <OtherProfiles activeProfile={activeProfile} userProfiles={userProfiles}
+                             setIsDropdownOpen={setIsDropdownOpen}/>
+            )}
           </div>
-          {userProfilesLoadingStatus === 'success' && userProfiles.length > 1 && (
-            <OtherProfiles activeProfile={activeProfile} userProfiles={userProfiles}
-                           setIsDropdownOpen={setIsDropdownOpen}/>
-          )}
         </div>
       )}
     </div>
