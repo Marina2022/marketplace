9f66ed8c7efb104e8ea638f2fef7aae9e58789f8
Marina2022 <marusiiiia@yandex.ru>
2024-09-05 14:37:42 +0300

    user profile form ready

--- src/components/lk-InnerPages/LkMainPage/ProfileTab/UserProfile/UserProfile.jsx
+++ src/components/lk-InnerPages/LkMainPage/ProfileTab/UserProfile/UserProfile.jsx
@@ -4,17 +4,15 @@ import pencil from '@/assets/img/lk/lk-main/pencil.svg';
 import {useForm} from 'react-hook-form';
 import {useDispatch, useSelector} from "react-redux";
 import {getUser, getUserData} from "@/store/userSlice.js";
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import InputMask from 'react-input-mask';
 import Button from "@/components/ui/Button/Button.jsx";
 
 const UserProfile = () => {
-  const userData = useSelector(getUserData);
-  const [textareaFio, setTextareaFio] = useState(null);
+  const userData = useSelector(getUserData);  
   const [textareaAddress, setTextareaAddress] = useState(null);
 
   const [editing, setEditing] = useState(false)
-
   const adjustTextareaHeight = (event) => {
     const textarea = event.target;
     textarea.style.height = `${textarea.scrollHeight}px`; // Устанавливаем новую высоту
@@ -23,29 +21,22 @@ const UserProfile = () => {
   useEffect(() => {
 
     setTimeout(() => {
-      if (textareaFio) {
-        adjustTextareaHeight({target: textareaFio}, true);
-      }
-
       if (textareaAddress) {
         adjustTextareaHeight({target: textareaAddress}, true);
       }
-
-    }, 100)
-
-
-  }, [textareaFio]);
-
+    }, 500)
+  }, [textareaAddress]);
 
   const {
-    register,
-    reset,
+    register,    
     handleSubmit,
     setValue,
     formState: {errors}
   } = useForm({
     defaultValues: {
-      name: '',
+      firstName: '',
+      secondName: '',
+      middleName: '',
       phone: '',
       email: '',
       whatsAppMsg: '',
@@ -55,44 +46,39 @@ const UserProfile = () => {
 
   useEffect(() => {
     if (userData) {
+
+      const nameParts = userData.fullName.split(' ')
+      const firstName = nameParts[0]  // Предположительно
+      const secondName = nameParts[2]
+      const middleName = nameParts[1]
+
+      
       const formattedPhone = `+7 (${userData.phoneNumber.slice(0, 3)}) ${userData.phoneNumber.slice(3, 6)}-${userData.phoneNumber.slice(6, 8)}-${userData.phoneNumber.slice(8)}`;
-      const formattedWatsup = `+7 (${userData.whatsAppMsg.slice(0, 3)}) ${userData.whatsAppMsg.slice(3, 6)}-${userData.whatsAppMsg.slice(6, 8)}-${userData.whatsAppMsg.slice(8)}`;
-      const formattedTg = `+7 (${userData.telegramMsg.slice(0, 3)}) ${userData.telegramMsg.slice(3, 6)}-${userData.telegramMsg.slice(6, 8)}-${userData.telegramMsg.slice(8)}`;
+            
+      const formattedWhatsApp = userData?.whatsAppMsg ? `+7 (${userData.whatsAppMsg.slice(0, 3)}) ${userData.whatsAppMsg.slice(3, 6)}-${userData.whatsAppMsg.slice(6, 8)}-${userData.whatsAppMsg.slice(8)}` : '';
+      const formattedTg = userData?.telegramMsg ? `+7 (${userData.telegramMsg.slice(0, 3)}) ${userData.telegramMsg.slice(3, 6)}-${userData.telegramMsg.slice(6, 8)}-${userData.telegramMsg.slice(8)}` : '';
+      
       setValue('email', userData.email ? userData.email.trim() : null);
       setValue('phone', formattedPhone);
-      setValue('whatsAppMsg', formattedWatsup);
+      setValue('whatsAppMsg', formattedWhatsApp);
       setValue('telegramMsg', formattedTg);
+      setValue('firstName', firstName);
+      setValue('secondName', secondName);
+      setValue('middleName', middleName);
     }
   }, [userData, setValue]);
 
   const dispatch = useDispatch()
-  const onSubmit = async (data) => {
-    // Преобразуем телефон в числовой формат для отправки
+  const onSubmit = async (data) => {    
     const numericPhone = data.phone.replace(/\D/g, '').slice(1);
-    // console.log("Отправляемый номер:", numericPhone);
-
     const numericWhatsApp = data.whatsAppMsg.replace(/\D/g, '').slice(1);
     const numericTelegram = data.telegramMsg.replace(/\D/g, '').slice(1);
-    
-    console.log(data);
-    console.log('fio: ', textareaFio.value)
-    console.log('address: ', textareaAddress.value)
-    console.log({numericPhone, numericWhatsApp, numericTelegram})
 
-    // "firstName": "string",
-    //   "secondName": "string",
-    //   "middleName": "string",
-    // Вероника Андреевна Певченко
-    
-    const nameParts = textareaFio.value.split(' ')
-    const firstName = nameParts[0]  // Предположительно
-    const secondName = nameParts[2]
-    const middleName = nameParts[1]
-    
     const body = {
       userId: userData.userId,
-      firstName, secondName, middleName,
-      
+      firstName: data.firstName, 
+      secondName: data.secondName, 
+      middleName: data.middleName,
       email: data.email,
       phoneNumber: numericPhone,
       whatsApp: numericWhatsApp,
@@ -101,23 +87,19 @@ const UserProfile = () => {
     }
 
     try {
-      const resp  = await axios.post('user', body)
+      const resp = await axios.post('user', body)
       console.log(resp)
       dispatch(getUser())
-    } catch(err) {
+      setEditing(false)
+    } catch (err) {
       console.log(err)
     }
-    
-    
-    
-    
-    
+
 
     console.log(body)
   };
 
 
-  
   return (
     <div className={s.profile}>
 
@@ -134,38 +116,62 @@ const UserProfile = () => {
           {
             editing && <Button className={s.saveBtn}>Сохранить&nbsp;изменения</Button>
           }
-  
+
         </div>
 
         <h2 className={s.subTitle}>Основная информация</h2>
 
         <div className={s.fieldset}>
           {/* ФИО */}
+
+          {/* FirstName */}
           <div className={s.control}>
-            <label
-              className={s.label} htmlFor="name">ФИО</label>
-            
-            <textarea
+            <label className={s.label} htmlFor="firstName">Имя</label>
+            <input
               disabled={!editing}
-              className={ !editing ? s.textareaDisabled : s.textarea}
+              className={!editing ? s.inputDisabled : s.input}
               placeholder="Не заполнено"
-              id="name"
-              spellCheck={false}
-              onChange={(e) => adjustTextareaHeight(e)}
-              defaultValue={userData?.fullName}
-              ref={(el) => {
-                setTextareaFio(el)
-              }}
-            ></textarea>
-            {errors.name && <p>{errors.name.message}</p>}
+              id="firstName"
+              type="text"
+              {...register('firstName')}
+            />
+            {errors.firstName && <p>{errors.firstName.message}</p>}
           </div>
 
+          {/* secondName */}
+          <div className={s.control}>
+            <label className={s.label} htmlFor="secondName">Фамилия</label>
+            <input
+              disabled={!editing}
+              className={!editing ? s.inputDisabled : s.input}
+              placeholder="Не заполнено"
+              id="secondName"
+              type="text"
+              {...register('secondName')}
+            />
+            {errors.secondName && <p>{errors.secondName.message}</p>}
+          </div>
+
+          {/* middleName */}
+          <div className={s.control}>
+            <label className={s.label} htmlFor="middleName">Отчество</label>
+            <input
+              disabled={!editing}
+              className={!editing ? s.inputDisabled : s.input}
+              placeholder="Не заполнено"
+              id="middleName"
+              type="text"
+              {...register('middleName')}
+            />
+            {errors.middleName && <p>{errors.middleName.message}</p>}
+          </div>
+          
           {/* Телефон */}
           <div className={s.control}>
             <label className={s.label} htmlFor="phone">Номер&nbsp;телефона</label>
             <InputMask
               disabled={!editing}
-              className={ !editing ? s.inputDisabled : s.input}              
+              className={!editing ? s.inputDisabled : s.input}
               mask="+7 (999) 999-99-99"
               {...register('phone', {
                 required: 'Поле телефон обязательно',
@@ -187,7 +193,7 @@ const UserProfile = () => {
             <label className={s.label} htmlFor="email">Email</label>
             <input
               disabled={!editing}
-              className={ !editing ? s.inputDisabled : s.input}              
+              className={!editing ? s.inputDisabled : s.input}
               placeholder="Не заполнено"
               id="email"
               type="email"
@@ -204,7 +210,7 @@ const UserProfile = () => {
 
             <textarea
               disabled={!editing}
-              className={ !editing ? s.textareaDisabled : s.textarea}              
+              className={!editing ? s.textareaDisabled : s.textarea}
               placeholder="Не заполнено"
               id="name"
               spellCheck={false}
@@ -223,11 +229,12 @@ const UserProfile = () => {
           <div className={s.control}>
             <label className={s.label} htmlFor="phone">Whatsapp</label>
             <InputMask
+              placeholder="Не заполнено"
               disabled={!editing}
-              className={ !editing ? s.inputDisabled : s.input}              
+              className={!editing ? s.inputDisabled : s.input}
               mask="+7 (999) 999-99-99"
               {...register('whatsAppMsg')}
-              defaultValue={userData ? `+7 (${userData.whatsAppMsg.slice(0, 3)}) ${userData.whatsAppMsg.slice(3, 6)}-${userData.whatsAppMsg.slice(6, 8)}-${userData.whatsAppMsg.slice(8)}` : ''} // Начальное значение
+              defaultValue={userData?.whatsAppMsg ? `+7 (${userData.whatsAppMsg.slice(0, 3)}) ${userData.whatsAppMsg.slice(3, 6)}-${userData.whatsAppMsg.slice(6, 8)}-${userData.whatsAppMsg.slice(8)}` : ''} // Начальное значение
             >
               {(inputProps) => (
                 <input
@@ -241,11 +248,12 @@ const UserProfile = () => {
           <div className={s.control}>
             <label className={s.label} htmlFor="phone">Telegram</label>
             <InputMask
+              placeholder="Не заполнено"
               disabled={!editing}
-              className={ !editing ? s.inputDisabled : s.input}              
+              className={!editing ? s.inputDisabled : s.input}
               mask="+7 (999) 999-99-99"
               {...register('telegramMsg')}
-              defaultValue={userData ? `+7 (${userData.telegramMsg.slice(0, 3)}) ${userData.telegramMsg.slice(3, 6)}-${userData.telegramMsg.slice(6, 8)}-${userData.telegramMsg.slice(8)}` : ''} // Начальное значение
+              defaultValue={userData?.telegramMsg ? `+7 (${userData?.telegramMsg.slice(0, 3)}) ${userData.telegramMsg.slice(3, 6)}-${userData.telegramMsg.slice(6, 8)}-${userData.telegramMsg.slice(8)}` : ''} // Начальное значение
             >
               {(inputProps) => (
                 <input
@@ -267,7 +275,7 @@ const UserProfile = () => {
             <label className={s.label} htmlFor="phone">Текущий тариф</label>
             <input
               disabled={!editing}
-              className={ !editing ? s.inputDisabled : s.input}              
+              className={!editing ? s.inputDisabled : s.input}
               placeholder="Не выбран"
               id="tariff"
               type="text"
