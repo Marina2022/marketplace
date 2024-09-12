37688bb50c7bfe349fd967346b46bae07f0a2eca
Marina2022 <marusiiiia@yandex.ru>
2024-09-12 14:27:41 +0300

    form basic setup

--- src/components/ProductPage/CreateReview/CreateReview.jsx
+++ src/components/ProductPage/CreateReview/CreateReview.jsx
@@ -4,35 +4,36 @@ import {Link, useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "@/api/axiosInstance.js";
 import Spinner from "@/components/ui/Spinner/Spinner.jsx";
+import {BASE_URL} from "@/consts/baseURL.js";
+import ReviewForm from "@/components/ProductPage/CreateReview/ReviewForm/ReviewForm.jsx";
 
 const CreateReview = () => {
   const {slug} = useParams()
 
-  const [product, setProduct] = useState(null)  
+  const [product, setProduct] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
 
   const navigate = useNavigate()
-  
+
   useEffect(() => {
     const getEligibility = async () => {
 
       try {
-        const resp1 = await axios(`/products/${slug}`)                
+        const resp1 = await axios(`/products/${slug}`)
         const resp2 = await axios.post('reviews/eligibility', {productId: resp1.data.productId})
-        
-        if (!resp2.data.isEligible) {          
+
+        if (!resp2.data.isEligible) {
           navigate('/')
           return
         }
         setProduct(resp1.data)
-        
+
       } catch (err) {
         console.log(err)
       } finally {
         setIsLoading(false)
       }
     }
-
     getEligibility()
   }, [])
 
@@ -40,11 +41,20 @@ const CreateReview = () => {
 
   return (
     <div className="container-review">
-
       <Link className={s.backLink} to={`/product/${slug}`}>
         <img src={backArror} alt="back arror"/>
         <span>Назад к карточке товара</span>
       </Link>
+      <div className={s.header}>
+        <div className={s.imageWrapper}>
+          <img className={s.headerImage} src={`${BASE_URL}${product.productImages[0].imageUrl}`}
+               alt={product.productImages[0].imageName}/>
+        </div>
+        <span className={s.headerText}>Отзыв о товаре {product.productName.slice(0, 33) + '...'} {}</span>
+
+      </div>
+      
+      <ReviewForm productId = {product.productId} />
 
     </div>
   );
---
+++ src/components/ProductPage/CreateReview/ReviewForm/ReviewForm.jsx
@@ -0,0 +1,11 @@
+import s from './ReviewForm.module.scss';
+
+const ReviewForm = () => {
+  return (
+    <form className={s.form}>
+      ReviewForm
+    </form>
+  );
+};
+
+export default ReviewForm;
