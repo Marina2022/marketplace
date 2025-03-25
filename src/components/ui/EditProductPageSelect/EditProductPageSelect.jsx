c8730f6618042b71f549574022e16893e12b3faa
Marina2022 <marusiiiia@yandex.ru>
2025-03-25 14:05:41 +0300

    fix

--- src/components/ui/EditProductPageSelect/EditProductPageSelect.jsx
+++ src/components/ui/EditProductPageSelect/EditProductPageSelect.jsx
@@ -17,11 +17,11 @@ const EditProductPageSelect = forwardRef(({
                                             placeholder = '',
                                             required,
                                             isVariant
-  
+
 
                                           }, ref) => {
 
-    const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState(false);
   const handleClick = () => {
     setEditing(prev => {
         if (prev === true) {
@@ -38,43 +38,46 @@ const EditProductPageSelect = forwardRef(({
   const handleBlur = () => {
     trigger(name)
     setEditing(false)
-  }  
-  
+  }
+
   const handleOptionClick = (option) => {
     setValue(name, {...option, isVariant})
     setEditing(false)
   }
-  
+
   return (
     <div>
       <div onBlur={handleBlur} tabIndex={0}>
-                
-        <div onClick={handleClick} className={editing ? s.selectFocused : isError ? s.errorField : s.notEditing}>
 
-          <svg className={editing ? s.dropdownIconOpened : s.dropdownIcon} width="18" height="9" viewBox="0 0 18 9"
-               fill="none"
-               xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M9.00344 8.80001C8.30344 8.80001 7.60344 8.53001 7.07344 8.00001L0.553438 1.48001C0.263438 1.19001 0.263438 0.710015 0.553438 0.420015C0.843437 0.130015 1.32344 0.130015 1.61344 0.420015L8.13344 6.94001C8.61344 7.42001 9.39344 7.42001 9.87344 6.94001L16.3934 0.420015C16.6834 0.130015 17.1634 0.130015 17.4534 0.420015C17.7434 0.710015 17.7434 1.19001 17.4534 1.48001L10.9334 8.00001C10.4034 8.53001 9.70344 8.80001 9.00344 8.80001Z"
-              fill="#658092"/>
-          </svg>
+        <div className={s.wrapperForIsVariant}>
+                    
+          <div onClick={handleClick} className={editing ? s.selectFocused : isError ? s.errorField : s.notEditing}>
 
-          <span className={s.inputValue}>
-          {
-            getValues(name) && getValues(name).value
-          }
-        </span>
+            <svg className={editing ? s.dropdownIconOpened : s.dropdownIcon} width="18" height="9" viewBox="0 0 18 9"
+                 fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M9.00344 8.80001C8.30344 8.80001 7.60344 8.53001 7.07344 8.00001L0.553438 1.48001C0.263438 1.19001 0.263438 0.710015 0.553438 0.420015C0.843437 0.130015 1.32344 0.130015 1.61344 0.420015L8.13344 6.94001C8.61344 7.42001 9.39344 7.42001 9.87344 6.94001L16.3934 0.420015C16.6834 0.130015 17.1634 0.130015 17.4534 0.420015C17.7434 0.710015 17.7434 1.19001 17.4534 1.48001L10.9334 8.00001C10.4034 8.53001 9.70344 8.80001 9.00344 8.80001Z"
+                fill="#658092"/>
+            </svg>
 
-          {
-            !getValues(name) &&
-            <div className={s.empty}><span> {placeholder}</span>
+            <span className={s.inputValue}>
+            {
+              getValues(name) && getValues(name).value
+            }
+          </span>
 
-              {
-                required && <span className={s.requiredStar}>*</span>
-              }
+            {
+              !getValues(name) &&
+              <div className={s.empty}><span> {placeholder}</span>
 
-            </div>
-          }
+                {
+                  required && <span className={s.requiredStar}>*</span>
+                }
+
+              </div>
+            }
+          </div>
         </div>
 
         {
@@ -86,7 +89,7 @@ const EditProductPageSelect = forwardRef(({
             <ul className={`${s.dropdownInner} lk-scroll`}
 
                 ref={(node) => {
-                  if (node) {                    
+                  if (node) {
                     if (node.scrollHeight < 472) node.style.width = '100%'
                   }
                 }}
@@ -96,7 +99,7 @@ const EditProductPageSelect = forwardRef(({
                 data.options.map(option => {
                   return (
                     <li key={option.valueId}
-                        onClick={()=>handleOptionClick(option)}
+                        onClick={() => handleOptionClick(option)}
                         className={s.selectOption}
                     >
                       {option.value}
