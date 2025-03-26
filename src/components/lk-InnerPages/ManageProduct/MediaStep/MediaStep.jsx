a40d4ae768ef507244cc2afb3550e4463f9be19e
Marina2022 <marusiiiia@yandex.ru>
2025-03-26 14:51:35 +0300

    desktop layout

--- src/components/lk-InnerPages/ManageProduct/MediaStep/MediaStep.jsx
+++ src/components/lk-InnerPages/ManageProduct/MediaStep/MediaStep.jsx
@@ -1,9 +1,28 @@
 import s from './MediaStep.module.scss';
 
 const MediaStep = () => {
+
+  const placeholders = Array.from({length: 15})
+
   return (
     <div>
       <h2 className={s.title}>Медиа</h2>
+
+      <h3 className={s.subtitle}>Фотографии товара</h3>
+
+      <ul className={s.photosWrapper}>
+        {
+          placeholders.map((placeholder, i) => {
+          return  (
+            
+            <li key={i} className={s.placeholder}>
+              {i}
+            </li>
+            
+          )
+          })
+        }
+      </ul>
     </div>
   );
 };
